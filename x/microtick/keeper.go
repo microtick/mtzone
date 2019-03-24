699853f8bc5eb6d9137094ccf7971cf49e3cc7f9
Mark Jackson <mjackson001@gmail.com>
2019-03-24 15:26:22 +0000

    initial build with accounts, staking and auth

---
+++ x/microtick/keeper.go
@@ -0,0 +1,22 @@
+package microtick
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type Keeper struct {
+	coinKeeper bank.Keeper
+	storeKey  sdk.StoreKey 
+	cdc *codec.Codec 
+}
+
+func NewKeeper(coinKeeper bank.Keeper, storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
+	return Keeper{
+		coinKeeper: coinKeeper,
+		storeKey:   storeKey,
+		cdc:        cdc,
+	}
+}
