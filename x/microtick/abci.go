1d4f636275aa45f9c90bfc6752746141751853ef
Mark <mjackson@microtick.com>
2020-06-29 13:53:53 -0600

    rename handler.go -> abci.go

---
+++ x/microtick/abci.go
@@ -0,0 +1,29 @@
+package microtick
+
+import (
+    "fmt"
+    "os"
+    "time"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    
+    mt "github.com/mjackson001/mtzone/x/microtick/types"
+    "github.com/mjackson001/mtzone/x/microtick/keeper"
+)
+
+func EndBlocker(ctx sdk.Context, mtKeeper keeper.Keeper) {
+    // Add commissions
+    mtKeeper.Sweep(ctx)
+    
+    // Monitor for end of chain
+    haltTime := mtKeeper.GetHaltTime(ctx)
+    now := ctx.BlockHeader().Time.UTC().Unix()
+    
+    //fmt.Printf("Halt Time: %d\n", haltTime)
+    //fmt.Printf("Now:       %d\n", now)
+    
+    if now >= haltTime {
+   	    fmt.Printf("Reached prearranged chain end time: %s\n", time.Unix(haltTime, 0).UTC().Format(mt.TimeFormat))
+   	    fmt.Println("Halting")
+	    os.Exit(7)
+    }
+}
