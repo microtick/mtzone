2592f35a9abfe76e8a56581c36a0325d2a85da8b
Mark <mjackson@microtick.com>
2021-04-16 07:13:11 -0600

    add American style option settlement

--- x/microtick/msg/txSettle.go
+++ x/microtick/msg/txSettle.go
@@ -12,10 +12,10 @@ import (
 )
 
 // The rules for settling a trade are as follows:
-// If European mode is set, (mode TBD)
+// If European mode is set, 
 //     If expiration time is now or in the past
 //         Anyone can settle-trade, value is paid out and difference refunded
-// If American mode is set, (TBD)
+// If American mode is set,
 //     If expiration time is in the future
 //         Only the buyer can settle trade (must be signed and verified)
 //     Else
@@ -52,10 +52,26 @@ func HandleTxSettleTrade(ctx sdk.Context, mtKeeper keeper.Keeper, params mt.Micr
     
     var settleLegData []SettleLegData
     
-    // check if trade has expired
     now := ctx.BlockHeader().Time
-    if now.Before(time.Unix(trade.Expiration, 0)) {
-        return nil, sdkerrors.Wrap(mt.ErrTradeSettlement, "trade not expired")
+    if params.EuropeanOptions {
+        
+        // European options:
+        // check if trade has expired - any party may settle (including a third party wanting to collect the settle incentive)
+        if now.Before(time.Unix(trade.Expiration, 0)) {
+            return nil, sdkerrors.Wrap(mt.ErrTradeSettlement, "European option - trade not expired")
+        }
+        
+    } else {
+        
+        // American options:
+        // prior to expiration, only taker may settle
+        // post-expiration, any party may settle (including a third party wanting to collect the settle incentive)
+        if now.Before(time.Unix(trade.Expiration, 0)) {
+            if msg.Requester.String() != trade.Taker.String() {
+                return nil, sdkerrors.Wrap(mt.ErrNotOwner, "American option - only taker may settle prior to expiration")
+            }
+        }
+        
     }
         
     dataMarket, err := mtKeeper.GetDataMarket(ctx, trade.Market)
@@ -88,52 +104,44 @@ func HandleTxSettleTrade(ctx sdk.Context, mtKeeper keeper.Keeper, params mt.Micr
     if err != nil {
         return nil, err
     }
-    
-    if params.EuropeanOptions {
         
-        // Payout and refunds
-        for _, pair := range settlements {
-            
-            // Long
-            err = mtKeeper.DepositMicrotickCoin(ctx, pair.SettleAccount, pair.Settle)
-            if err != nil {
-                return nil, sdkerrors.Wrap(mt.ErrTradeSettlement, "payout")
-            }
-            
-            // Refund
-            err := mtKeeper.DepositMicrotickCoin(ctx, pair.RefundAccount, pair.Refund)
-            if err != nil {
-                return nil, sdkerrors.Wrap(mt.ErrTradeSettlement, "refund")
-            }
-            
-            // Adjust trade backing
-            accountStatus := mtKeeper.GetAccountStatus(ctx, pair.RefundAccount)
-            accountStatus.ActiveTrades.Delete(trade.Id)
-            accountStatus.TradeBacking = accountStatus.TradeBacking.Sub(pair.Backing)
-            mtKeeper.SetAccountStatus(ctx, pair.RefundAccount, accountStatus)
-            
-            accountStatus = mtKeeper.GetAccountStatus(ctx, pair.SettleAccount)
-            accountStatus.ActiveTrades.Delete(trade.Id)
-            mtKeeper.SetAccountStatus(ctx, pair.SettleAccount, accountStatus)
-            
-            settleLegData = append(settleLegData, SettleLegData {
-                LegId: pair.LegId,
-                SettleAccount: pair.SettleAccount,
-                Settle: pair.Settle,
-                RefundAccount: pair.RefundAccount,
-                Refund: pair.Refund,
-            })
+    // Payout and refunds
+    for _, pair := range settlements {
+        
+        // Long payout
+        err = mtKeeper.DepositMicrotickCoin(ctx, pair.SettleAccount, pair.Settle)
+        if err != nil {
+            return nil, sdkerrors.Wrap(mt.ErrTradeSettlement, "payout")
         }
+        
+        // Refund
+        err := mtKeeper.DepositMicrotickCoin(ctx, pair.RefundAccount, pair.Refund)
+        if err != nil {
+            return nil, sdkerrors.Wrap(mt.ErrTradeSettlement, "refund")
+        }
+        
+        // Adjust trade backing
+        accountStatus := mtKeeper.GetAccountStatus(ctx, pair.RefundAccount)
+        accountStatus.ActiveTrades.Delete(trade.Id)
+        accountStatus.TradeBacking = accountStatus.TradeBacking.Sub(pair.Backing)
+        mtKeeper.SetAccountStatus(ctx, pair.RefundAccount, accountStatus)
+        
+        accountStatus = mtKeeper.GetAccountStatus(ctx, pair.SettleAccount)
+        accountStatus.ActiveTrades.Delete(trade.Id)
+        mtKeeper.SetAccountStatus(ctx, pair.SettleAccount, accountStatus)
+        
+        settleLegData = append(settleLegData, SettleLegData {
+            LegId: pair.LegId,
+            SettleAccount: pair.SettleAccount,
+            Settle: pair.Settle,
+            RefundAccount: pair.RefundAccount,
+            Refund: pair.Refund,
+        })
         
         accountStatusTaker := mtKeeper.GetAccountStatus(ctx, trade.Taker)
         accountStatusTaker.SettleBacking = accountStatusTaker.SettleBacking.Sub(trade.SettleIncentive)
         mtKeeper.SetAccountStatus(ctx, trade.Taker, accountStatusTaker)
         mtKeeper.DeleteActiveTrade(ctx, trade.Id)
-        
-    } else {
-        
-        // American options not implemented yet
-        return nil, sdkerrors.Wrap(mt.ErrGeneral, "American style option settlement not implemented yet")
         
     }
     
